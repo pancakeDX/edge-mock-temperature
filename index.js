66f43194734de540cab737ce24b2a3891652a4cb
Wilson Liao <wilson.smliao@gmail.com>
2022-05-10 16:34:13 +0800

    change interval

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const mqtt = require('mqtt')
 /** 
  * -- Gets credentials from env variables
  */
-// const esServices = JSON.parse(process.env.ENSAAS_SERVICES);
-// const mqttUri = esServices['p-rabbitmq'][0].credentials.protocols.mqtt.uri
+const esServices = JSON.parse(process.env.ENSAAS_SERVICES);
+const mqttUri = esServices['p-rabbitmq'][0].credentials.protocols.mqtt.uri
 
 /** 
  * Use credentials directly from local device
  * Ispect the credentials inside the secret and fill out the following variables.
  */
 // ---mqtt---
-const username = '';
-const password = '';
-const externalHosts = '';
+// const username = '';
+// const password = '';
+// const externalHosts = '';
 
 
-const mqttUri = `mqtt://${username}:${password}@${externalHosts}:1883`;
+// const mqttUri = `mqtt://${username}:${password}@${externalHosts}:1883`;
 
 /** 
  * Connects to the IoTHub service using MQTT URI
@@ -25,16 +25,17 @@ const client = mqtt.connect(mqttUri);
 client.on('connect', (connack) => {
   setInterval(() => {
     publishMockTemp();
-  }, 3000);
+  }, 10000);
 });
 
 /** 
  * Publish mock random temperature periodically 
  */
 function publishMockTemp() {
-  const temp = Math.floor((Math.random() * 8) + 22);
+  const temp = Math.floor((Math.random() * 5) + 22);
+  const topicName = 'livingroom/temperature/wilson-liao';
 // Please fill in your own number, user can check it in in your mqtt pod, use $kubectl logs -f pod_name
-  client.publish('livingroom/temperature/{own number}', temp.toString(), { qos: 0 }, (err, packet) => {
-    if (!err) console.log('Data sent to livingroom/temperature -- ' + temp);
+  client.publish(topicName, temp.toString(), { qos: 0 }, (err, packet) => {
+    if (!err) console.log(`Data sent to ${topicName} -- ` + temp);
   });
 }
